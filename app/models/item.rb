0b53625685503d5bbfa11b623324504b03313559
dante <dantein88@gmail.com>
2012-07-20 22:20:49 -0400

    second commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
-  
+  belongs_to :user
+  #the self search code is for building the search list
   def self.search(query)
     if query
       find(:all ,:conditions=>['name LIKE ?', "%#{query}%"])
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120711162810) do
+ActiveRecord::Schema.define(:version => 20120719004005) do
 
   create_table "items", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.text     "pic"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
