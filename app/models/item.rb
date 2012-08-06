295a7e6751e791d938686c529fdc31dd4bcd00cf
dante <dantein88@gmail.com>
2012-08-05 23:44:13 -0400

    jqueryui removed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   has_many :images
   belongs_to :category
-  attr_accessible :category_id, :name, :popular
+  attr_accessible :category_id, :name, :popular, :price, :description
   
   #the self search code is for building the search list
   def self.search(query)
