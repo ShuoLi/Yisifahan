524a807cd532e1665ef23f3d57d04e121eff5642
Will Chou <chou3213@gmail.com>
2012-08-07 00:41:10 -0700

    comment out the image stuff, I will figure it out later

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -4,20 +4,19 @@ ActiveAdmin.register Category do
     column :created_at
     column :updated_at
 
-    column :image do 
-      form :html => { :multipart => true } do |f|
-
-        f.inputs "Image", :for => [:category_image, f.object.category_image || Category_image.new ] do |fm|
-          # fm.inputs "Image", :for => [:image, fm.object.image || Image.new] do |fmi|
-          fm.input :file, :for => :category_image, :as => :file, :hint => f.template.image_tag(f.object.category_image.image.url(:cropped))
-          # end
-          # fm.inputs "Video", :for => [:video, fm.object.video || Video.new] do |fmv|
-          # fmv.input :url
-          # end
-        end
-      end
-    end
-
+    # column :image do 
+    #   form :html => { :multipart => true } do |f|
+    # 
+    #     f.inputs "Image", :for => [:category_image, f.object.category_image || Category_image.new ] do |fm|
+    #       # fm.inputs "Image", :for => [:image, fm.object.image || Image.new] do |fmi|
+    #       fm.input :file, :for => :category_image, :as => :file, :hint => f.template.image_tag(f.object.category_image.image.url(:cropped))
+    #       # end
+    #       # fm.inputs "Video", :for => [:video, fm.object.video || Video.new] do |fmv|
+    #       # fmv.input :url
+    #       # end
+    #     end
+    #   end
+    # end
 
     default_actions
   end
