79c4a87bc4a47acf80525456e91c3c98fd5c5ed9
Kevin <kevinchh88@gmail.com>
2012-08-11 11:12:04 -0700

    some fixes, better page, problem with buy button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,13 @@ class HomeController < ApplicationController
   
   def index
     @slides = Slide.find(:all)
-	  @items = Item.where(:popular=>true)
+	@items = Item.where(:popular=>true)
+	  
+	if params.has_key?(:slide_id)
+	  	@current = Slide.where(:id=>params[:slide_id]).first
+  	else
+	  	@current = @slides[0]
+  	end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @items }
