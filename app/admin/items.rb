3b0543126f5871fcb2b349427f42a6863026af78
dante <dantein88@gmail.com>
2012-08-17 16:44:43 -0400

    css of item_box

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,9 +1,9 @@
 ActiveAdmin.register Item do
-	controller do 
-		def new
-			@item = Item.new
-		end
-	end
-
-	form :partial => "admin/image_upload_form"
+  # controller do 
+  #   def new
+  #     @item = Item.new
+  #   end
+  # end
+  # 
+  # form :partial => "admin/image_upload_form"
 end
