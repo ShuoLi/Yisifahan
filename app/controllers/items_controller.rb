6296d3570490d5aa59522455053876cbd14f6724
Kevin Huang <kevinchh88@gmail.com>
2012-08-17 16:12:15 -0700

    Test Changes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,28 +3,24 @@
   # GET /items
   # GET /items.json
   def index
+  	parameters = {}  	
   	if (params.has_key?(:ascend))
-  		parameters = {"ascend"=>params[:ascend]}
-  		if (params.has_key?(:cpath))
-  			parameters["cpath"] = params[:cpath]
-  		elsif (params.has_key?(:search_text))
-  			parameters["search_text"] = params[:search_text]			
-  		elsif (params.has_key?(:low) or params.has_key?(:high))
-  			parameters["low"] = params[:low]
-  			parameters["high"] = params[:high]
-  		end
-  		@items = Item.sortbyprice(parameters)
-  		
-  		
-  	elsif (params.has_key?(:search_text))
-  		@items = Item.search(params[:search_text])
-  	elsif (params.has_key?(:low) or params.has_key?(:high))
-  		@items = Item.filter(params[:low], params[:high])
-  	elsif (params.has_key?(:cpath))
-  		@items = Item.categoryfilter(params[:cpath])
-  	else
-  		@items = Item.find(:all)
+  		parameters["ascend"] = params[:ascend]
+	end
+  	if (params.has_key?(:search_text))
+  		parameters["search_text"] = params[:search_text]
   	end
+  	if (params.has_key?(:low))
+  		parameters["low"] = params[:low]
+  	end
+  	if (params.has_key?(:high))
+  	    parameters["high"] = params[:high]
+  	end
+  	if (params.has_key?(:cpath))
+  		parameters["cpath"] = params[:cpath]
+	end
+	
+	@items = Item.findItems(parameters)
 
   	perpage = 10
   	@numpages = (@items.count/perpage)
@@ -46,6 +42,27 @@
     end
   end
 
+  # render message to send wishlist
+  def send_wishlist
+    if cookies['wishlist']
+      wishlist_ids = cookies['wishlist'].split(',')
+      wishlist = []
+      wishlist_ids.each do |id|
+        wishlist << Item.find(id)
+      end
+      if !wishlist.empty?
+        @msg = "I am interested in these items:\r\n"
+        wishlist.each do |item|
+          @msg += item.name + ", item code: #" + item.item_code.to_s + ";\r\n"
+        end
+      end
+      # puts @msg
+      render "send_wishlist.js.erb"
+    else
+      render :nothing => true
+    end
+  end
+
   # GET /items/1
   # GET /items/1.json
   def show
@@ -58,9 +75,7 @@
     else
     	@current = nil
     end
-    # @similar = Item.find(:all, :conditions=>["category_id IS ? AND id IS NOT ?", @item.category_id, @item.id])
-    @similar = @item.category.items.to_a
-    @similar.delete(@item)
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @item }
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,81 +7,48 @@ class Item < ActiveRecord::Base
   validates :price, :presence => true
   validates :name, :presence => true
   validates :item_code, :presence => true, :uniqueness => true
-
-  #the self search code is for building the search list
-  def self.search(query)
-    if query
-      result = where('name LIKE ?', "%#{query}%")
-      # find by item_code
-      item = find_by_item_code(query)
-      result << item if item
-    else
-      result = find(:all)
-    end
-    return result
-  end
   
-  def self.filter(low, high)
+  def self.findItems(parameters)
   	condition = ""
-  	if low != "" and low
-  		condition += ' price >= ' + low
-  	end
-  		
-  	if high != "" and high
-  		if condition != ""
-  			condition += ' AND '
-  		end
-  		condition += ' price <= ' + high
-  	end
-  	where(condition).order('price ASC')
-  end
-  
-  def self.categoryfilter(category)
-   	if category == "-1"
-  		where('category_id IS null')
-	else
- 		where(category_id: category)
-  	end
-  end
-  
-  def self.sortbyprice(parameters)
-  	condition = ""
-  	if parameters["cpath"]
-  		if condition != ""
-  			condtion += ' AND '
-  		end
-  		if parameters["cpath"] == "-1"
-  			condition += "category_id IS null"
-  		else
-  			condition += "category_id = " + parameters["cpath"]
-  		end
-  	end
-  	
-  	if parameters["low"] != "" and parameters["low"]
-  		if condition != ""
-  			condition += ' AND '
-  		end
-   		condition += ' price >= ' + parameters["low"]
-  	end
-  		
-  	if parameters["high"] != "" and parameters["high"]
-  		if condition != ""
-  			condition += ' AND '
-  		end
-  		condition += ' price <= ' + parameters["high"]
-  	end
-  	
   	order = ""
-  	if parameters["ascend"] == "true"
-  		order +='price ASC'
-  	else
-  		order +='price DESC'
+  	parameters.each do |p|
+  		case p.first
+  			when "ascend"
+  				if p[1] == "1"
+  					order += 'created_at DESC'
+  				elsif p[1] == "2"
+			  		order +='price ASC'
+			  	else
+			  		order +='price DESC'
+			  	end
+  			when "low"
+  				condition += self.addAND(condition)
+  				condition += ' price >= ' + p[1]
+  			when "high"
+  				condition += self.addAND(condition)
+  				condition += ' price <= ' + p[1]  				
+  			when "cpath"
+  				condition += self.addAND(condition)
+  				if p[1] == "-1"
+		  			condition += "category_id IS null"
+		  		else
+		  			condition += "category_id = " + p[1]
+		  		end    			
+  		end
   	end
   	
-  	if parameters["search_text"]
+   	if parameters["search_text"]
   		where('name LIKE ?', "%#{parameters["search_text"]}%").where(condition).order(order)
   	else
   		where(condition).order(order)
+  	end 	
+  end
+  
+  def self.addAND(condition)
+  	if condition != ""
+  		' AND '
+  	else
+  		""
   	end
   end
 end
