e1ef5ed30bf7ba1fc2caaf0464a71cb4a3375df4
dante <dantein88@gmail.com>
2012-08-19 20:15:32 -0400

    item detail add to wishlist works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,20 @@
 //= require bootstrap
 //= require_tree .
 
+$.ready(function(){
+$("#myCart").click(function(){
+	$('#wishlist').toggle("fast");
+});
 
 
+});
 
+function add_item_in_wishlist_html(item_id){
+	// display in wishlist
+	var item_name = $("#item_box_"+item_id+" .item_code").text();
+	var item_price = $("#item_box_"+item_id+" .item_price").text();
+	var imagelink = $("#item_box_"+item_id+" .item_pic").attr("src");
+	$('#wish_items').prepend("<span class='label remove-margin remove-border span2 pull-left'> #"+item_name+"<span class='pull-right'><i class='icon-remove'></i></span></span>");
+	$('#wish_items').prepend("<a href='/items/"+item_id+"'><img src="+imagelink+"/></a>")
+		
+}
