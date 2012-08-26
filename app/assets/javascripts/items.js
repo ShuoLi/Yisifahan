89a54dfb26679b4cc265f63a141f4787e5ea1bc9
dante <dantein88@gmail.com>
2012-08-26 19:40:19 -0400

    wishlist scope problem fixed

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,14 +2,14 @@ $.ready(function(){
 	$(".remove_wish_item").click(function(){
 		var s = this.id.toString();
 		var item_id = s.split('_')[1];
-		var wishlist = $.cookie('wishlist');
+		var wishlist = $.cookie('wishlist',{ path: '/' });
 		wishlist = wishlist.split(',');
 		//remove one element from the wishlist
 		while (wishlist.indexOf(item_id) !== -1) {
 			wishlist.splice(wishlist.indexOf(item_id), 1);
 		}
 	
-		$.cookie('wishlist', wishlist, { expires: 99 });
+		$.cookie('wishlist', wishlist, { expires: 99  ,path: '/' });
 
 		$('#interest_'+item_id).removeClass('btn-danger').addClass('btn-success');
 		$('#showInterest_'+item_id).removeClass('btn-danger').addClass('btn-success');
