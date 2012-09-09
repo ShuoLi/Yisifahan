7fb5936e09a5acb32c933aace70603835ebc2627
Shuo <Shuo@Shuos-MacBook-Pro.local>
2012-09-08 21:39:33 -0400

    all

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -2,5 +2,5 @@ class Slide < ActiveRecord::Base
   belongs_to :item
   attr_accessible :item_id, :image
   mount_uploader :image, ImageUploader
-  validates_presence_of :image
+  validates_presence_of :image, :item_id
 end
